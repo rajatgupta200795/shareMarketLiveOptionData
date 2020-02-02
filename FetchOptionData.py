95410908fa3079e7e27084bec46fd59b25e6d3ef
Rajat Gupta <rajatgupta200795@gmail.com>
2020-02-02 21:50:30 +0530

    Update FetchOptionData.py

--- FetchOptionData.py
+++ FetchOptionData.py
@@ -17,7 +17,13 @@ def fetchOptionData(url, expiry_date):
 
     base_url = (url)
 
-    page = requests.get(base_url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+        "Upgrade-Insecure-Requests": "1", "DNT": "1",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate"}
+
+    page = requests.get(base_url, headers=headers)
     page.status_code
     page.content
 
