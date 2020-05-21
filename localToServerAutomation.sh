f84cca8d1dd2ea716103f62f6c59872e223991d3
Rajat Gupta <rajatgupta200795>
2020-05-21 21:58:18 +0530

    adding localToserver file

---
+++ localToServerAutomation.sh
@@ -0,0 +1,7 @@
+#~/shareMarketLiveOptionData/serverDbInsert.sh > shareMarketLiveOptionData/running_query_logs.txt
+~/shareMarketLiveOptionData/serverDbInsert.sh 
+#$(pwd)/DbInsert.sh 
+
+#~/shareMarketLiveOptionData/test.sh
+#~/shareMarketLiveOptionData/Running_Expiry_Date.txt
+
--- optionAutomationLocalServer.sh
+++ optionAutomationLocalServer.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-while  true
+while true
 do
 
 echo "Fetching CSV files";
-python3 ~/Downloads/shareMarketLiveOptionData/FetchOptionData.py
-sleep 2
+python3 /home/test/Downloads/shareMarketLiveOptionData/FetchOptionData.py
 
 echo "CSV files are fetched";
 echo "Uploading files to server";
-scp  -i ~/Downloads/key-couchbase-instance-sharesathi21.pem  ~/Downloads/shareMarketLiveOptionData/*.csv   sharesathi21@34.83.244.171:~/shareMarketLiveOptionData/
+scp -i /home/test/Downloads/key-couchbase-instance-sharesathi21.pem /home/test/Downloads/shareMarketLiveOptionData/*.csv sharesathi21@34.83.244.171:~/shareMarketLiveOptionData/
 
 echo "Files are uploaded successfully"
 echo "Inserting data into server database"
-ssh -i ~/Downloads/key-couchbase-instance-sharesathi21.pem sharesathi21@34.83.244.171 shareMarketLiveOptionData/localToServerAutomation.sh &
+ssh -i /home/test/Downloads/key-couchbase-instance-sharesathi21.pem sharesathi21@34.83.244.171 shareMarketLiveOptionData/localToServerAutomation.sh &
 
 echo "Inserting data into local database"
 
-~/Downloads/shareMarketLiveOptionData/DbInsert.sh
+/home/test/Downloads/shareMarketLiveOptionData/DbInsert.sh
 
 m=10#$(date +"%M");
 s=10#$(date +"%S");
@@ -39,7 +38,7 @@ fi
 
 
 if [ "$rmin" -eq 3 ]; then
-totalSec=$((  120 + $rsec )) 
+totalSec=$(( 120 + $rsec )) 
 echo $totalSec
 fi
 
